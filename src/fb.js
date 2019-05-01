2ae2384fbcece16e2d30141b70c61474891c0a98
mailbagrahul <raaghul.u@gmail.com>
2019-05-01 10:03:06 +0530

    Update fb.js

--- src/fb.js
+++ src/fb.js
@@ -6,9 +6,7 @@ var config = {
   apiKey: "AIzaSyCzSO2ZgnY8QG_ojhGh_QtHom5s-XzV4FE",
   authDomain: "todo-ninja-87521.firebaseapp.com",
   databaseURL: "https://todo-ninja-87521.firebaseio.com",
-  projectId: "todo-ninja-87521",
-  storageBucket: "todo-ninja-87521.appspot.com",
-  messagingSenderId: "528807385326"
+  projectId: "todo-ninja-87521"
 };
 firebase.initializeApp(config);
 
