9061b340eb2277379850353a6d2366865405f7eb
mailbagrahul <raaghul.u@gmail.com>
2019-05-01 10:20:40 +0530

    Update fb.js

--- src/fb.js
+++ src/fb.js
@@ -3,10 +3,10 @@ import "firebase/firestore";
 
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyCzSO2ZgnY8QG_ojhGh_QtHom5s-XzV4FE",
-  authDomain: "todo-ninja-87521.firebaseapp.com",
-  databaseURL: "https://todo-ninja-87521.firebaseio.com",
-  projectId: "todo-ninja-87521"
+  apiKey: "",
+  authDomain: ",
+  databaseURL: "",
+  projectId: ""
 };
 firebase.initializeApp(config);
 
